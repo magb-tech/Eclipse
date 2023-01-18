af05797507d30fca5204e4072b8db694cdee2e22
magb-tech <magbsoy2@outlook.com>
2023-01-18 19:56:17 +0100

    Ejmplo 1 Repositorio Gib

---
+++ src/Holamundo/HolaMundo.java
@@ -0,0 +1,12 @@
+package Holamundo;
+
+public class HolaMundo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// ESCRIBIR "Hola mundo"
+		System.out.println("Hola Mundo");
+
+	}
+
+}
